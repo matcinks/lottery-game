71b2a3524305e18e8d8a1572de0ac7daafa5bbff
matcinks <x.mateuszstepien@gmail.com>
2024-04-26 14:13:22 +0200

    Refactor cron scheduled - added properties values for production and integration tests.

--- src/main/java/org/mateusz/infrastructure/numbergenerator/scheduler/WinningNumbersScheduler.java
+++ src/main/java/org/mateusz/infrastructure/numbergenerator/scheduler/WinningNumbersScheduler.java
@@ -14,7 +14,8 @@ public class WinningNumbersScheduler {
 
     private final WinningNumbersGeneratorFacade winningNumbersGeneratorFacade;
 
-    @Scheduled(cron = "*/10 * * * * *")
+//    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "${lotto.number-generator.lotteryRunOccurrence}")
     public void generateWinningNumbers() {
         log.info("Winning numbers scheduler enabled");
         WinningNumbersDto winningNumbersDto = winningNumbersGeneratorFacade.generateWinningNumbers();
